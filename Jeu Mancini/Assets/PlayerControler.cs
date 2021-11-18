7b94317deed6e67e24ed02dd97ebe8c9009486c1
Poldup <chretiennot.leopold@gmail.com>
2021-11-18 20:40:30 +0100

    Script déplacements et saut
    déplacement g/d
    saut rapide
    (reste double saut et collisions murs)

--- Jeu Mancini/Assets/PlayerControler.cs
+++ Jeu Mancini/Assets/PlayerControler.cs
@@ -5,172 +5,231 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    public float speed = 10.5f;
-    public float jumpStrength = 50.2f;
+    public float moveSpeed;
+    public float maxJumpSpeed;
+    public float jumpForce;
+    public float jumpDuration;
+
+
+    private float jumpTimer;
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
     public Rigidbody2D rb;
+    private Vector2 velocity = Vector2.zero;
 
-    public KeyCode rightKey;
-    public KeyCode leftKey;
-    public KeyCode jumpKey;
-
-    public bool grounded;
-    public bool onWallRigth;
-    public bool onWallLeft;
-
-    public float distance = 1.5f;
-    public float heigthDecal = -0.15f;
-
-    public float playerSize = 0.1f;
-
-    public float fallMultiplier = 1.1f;
-    public float maxFallSpeed = -10;
-
-    public Animator animator;
-    public SpriteRenderer spriterenderer;
-
-
-
-    void Update()
+    private void Update()
     {
-        WallRightDectection();
-        WallLeftDectection();
-        LateralMove();
-        DetectGround();
-        JumpMove();
-        FallAcceleration();
-        // ManageAnimations();
-
+        Jump();
     }
 
-    void WallRightDectection()
+    void FixedUpdate()
     {
-        Vector3 origin = transform.position + new Vector3(playerSize, heigthDecal, 0);
-        Vector3 direction = new Vector3(1, 0, 0);
-        Color rayColor;
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
 
-        if (Physics2D.Raycast(origin, direction, distance))
-        {
-            onWallRigth = true;
-            rayColor = Color.green;
-        }
-        else
-        {
-            onWallRigth = false;
-            rayColor = Color.red;
-        }
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
 
-        Debug.DrawRay(origin, direction * distance, rayColor);
+        MovePlayer(horizontalMovement);
     }
 
-    void WallLeftDectection()
+    void MovePlayer(float _horizontalMovement)
     {
-        Vector3 origin = transform.position + new Vector3(-playerSize, heigthDecal, 0);
-        Vector3 direction = new Vector3(-1, 0, 0);
-        Color rayColor;
-
-        if (Physics2D.Raycast(origin, direction, distance))
-        {
-            onWallLeft = true;
-            rayColor = Color.green;
-        }
-        else
-        {
-            onWallLeft = false;
-            rayColor = Color.red;
-        }
-
-        Debug.DrawRay(origin, direction * distance, rayColor);
+        Vector2 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
     }
 
-    void LateralMove()
+    void Jump()
     {
-        if (Input.GetKey(rightKey))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            if (onWallRigth == false)
+            isJumping = true;
+            jumpTimer = jumpDuration;
+            rb.velocity = Vector2.zero;
+            print(rb.velocity);
+        }
+        if(isJumping)
+        {
+            rb.velocity = transform.up * jumpForce;
+            jumpTimer -= Time.deltaTime;
+            print(rb.velocity);
+            if (jumpTimer<=0)
             {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
-            }
-        }
-
-        if (Input.GetKey(leftKey))
-        {
-            if (onWallLeft == false)
-            {
-                rb.velocity = new Vector2(-speed, rb.velocity.y);
+                isJumping = false;
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y * 1.1f, 0, maxJumpSpeed));
             }
         }
     }
-
-    void JumpMove()
-    {
-        if (grounded == true)
-        {
-            if (Input.GetKeyDown(jumpKey))
-            {
-                rb.AddForce(new Vector2(0, jumpStrength));
-            }
-        }
-    }
-
-    void DetectGround()
-    {
-        Vector3 origin = transform.position + new Vector3(0, heigthDecal, 0);
-        Vector3 direction = new Vector3(0, -1, 0);
-        Color rayColor;
-
-        if (Physics2D.Raycast(origin, direction, distance))
-        {
-            grounded = true;
-            rayColor = Color.green;
-        }
-        else
-        {
-            grounded = false;
-            rayColor = Color.red;
-        }
-
-        Debug.DrawRay(origin, direction * distance, rayColor);
-    }
-
-    void FallAcceleration()
-    {
-        if (rb.velocity.y < 0)
-        {
-
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y * 1.1f, maxFallSpeed, 0));
-        }
-    }
-
-    /*
-    void ManageAnimations()
-    {
-        if (rb.velocity.magnitude > 0.01f)
-        {
-            animator.Play("frogrun");
-        }
-        else
-        {
-            animator.Play("chickenidle");
-        }
-        if (Input.GetKey(leftKey))
-        {
-            spriterenderer.flipX = true;
-        }
-
-        if (Input.GetKey(rightKey))
-        {
-            spriterenderer.flipX = false;
-        }
-    }
-    
-
-    public void GameOver()
-    {
-        Debug.Log("Game Over");
-
-        string sceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-
-    }*/
 }
+
+
+/*public float speed = 10.5f;
+public float jumpStrength = 50.2f;
+public Rigidbody2D rb;
+
+public KeyCode rightKey;
+public KeyCode leftKey;
+public KeyCode jumpKey;
+
+public bool grounded;
+public bool onWallRigth;
+public bool onWallLeft;
+
+public float distance = 1.5f;
+public float heigthDecal = -0.15f;
+
+public float playerSize = 0.1f;
+
+public float fallMultiplier = 1.1f;
+public float maxFallSpeed = -10;
+
+public Animator animator;
+public SpriteRenderer spriterenderer;
+
+
+
+void Update()
+{
+    WallRightDectection();
+    WallLeftDectection();
+    LateralMove();
+    DetectGround();
+    JumpMove();
+    FallAcceleration();
+    // ManageAnimations();
+
+}
+
+void WallRightDectection()
+{
+    Vector3 origin = transform.position + new Vector3(playerSize, heigthDecal, 0);
+    Vector3 direction = new Vector3(1, 0, 0);
+    Color rayColor;
+
+    if (Physics2D.Raycast(origin, direction, distance))
+    {
+        onWallRigth = true;
+        rayColor = Color.green;
+    }
+    else
+    {
+        onWallRigth = false;
+        rayColor = Color.red;
+    }
+
+    Debug.DrawRay(origin, direction * distance, rayColor);
+}
+
+void WallLeftDectection()
+{
+    Vector3 origin = transform.position + new Vector3(-playerSize, heigthDecal, 0);
+    Vector3 direction = new Vector3(-1, 0, 0);
+    Color rayColor;
+
+    if (Physics2D.Raycast(origin, direction, distance))
+    {
+        onWallLeft = true;
+        rayColor = Color.green;
+    }
+    else
+    {
+        onWallLeft = false;
+        rayColor = Color.red;
+    }
+
+    Debug.DrawRay(origin, direction * distance, rayColor);
+}
+
+void LateralMove()
+{
+    if (Input.GetKey(rightKey))
+    {
+        if (onWallRigth == false)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+
+    if (Input.GetKey(leftKey))
+    {
+        if (onWallLeft == false)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+    }
+}
+
+void JumpMove()
+{
+    if (grounded == true)
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            rb.AddForce(new Vector2(0, jumpStrength));
+        }
+    }
+}
+
+void DetectGround()
+{
+    Vector3 origin = transform.position + new Vector3(0, heigthDecal, 0);
+    Vector3 direction = new Vector3(0, -1, 0);
+    Color rayColor;
+
+    if (Physics2D.Raycast(origin, direction, distance))
+    {
+        grounded = true;
+        rayColor = Color.green;
+    }
+    else
+    {
+        grounded = false;
+        rayColor = Color.red;
+    }
+
+    Debug.DrawRay(origin, direction * distance, rayColor);
+}
+
+void FallAcceleration()
+{
+    if (rb.velocity.y < 0)
+    {
+
+        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y * 1.1f, maxFallSpeed, 0));
+    }
+}
+
+/*
+void ManageAnimations()
+{
+    if (rb.velocity.magnitude > 0.01f)
+    {
+        animator.Play("frogrun");
+    }
+    else
+    {
+        animator.Play("chickenidle");
+    }
+    if (Input.GetKey(leftKey))
+    {
+        spriterenderer.flipX = true;
+    }
+
+    if (Input.GetKey(rightKey))
+    {
+        spriterenderer.flipX = false;
+    }
+}
+
+
+public void GameOver()
+{
+    Debug.Log("Game Over");
+
+    string sceneName = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+}*/
 
